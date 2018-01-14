5b45931e7d1af2545660050caaea237f3b4bb9c7
Simonas Drungilas <s.drungilas@gmail.com>
2018-01-14 10:05:56 +0200

    Week3: Rework Point class in provided scaffolding

--- week3/Point.java
+++ week3/Point.java
@@ -1,117 +1,143 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *  
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
 import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class Point implements Comparable<Point> {
-  private final int x;
-  private final int y;
-  
-  public Point(int x, int y) {
-    if (x < 0 || y < 0) {
-      throw new java.lang.NullPointerException();
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+    private static final double epsilon = 1 / 32768;
+
+    /**
+     * Initializes a new point.
+     *
+     * @param  x the <em>x</em>-coordinate of the point
+     * @param  y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
     }
 
-    this.x = x;
-    this.y = y;
-  }
-
-  public int compareTo(Point that) {
-    if (this.y < that.y) { return -1; }
-
-    if (this.y == that.y) {
-      if (this.x == that.x) { return 0; }
-      if (this.x < that.x) { return -1; }
-    }
-    
-    return 1;
-  }
-
-  /**
-  * antisymmetric,
-  where p and q have coordinates in [0, 500)
-  Failed on trial 58 of 100000 p = (324, 99) q = (66, 99) p.compareTo(q) = 1 q.compareTo(p) = 1
-   */
-
-  public double slopeTo(Point that) {
-    if(this.compareTo(that) == 0) { return Double.NEGATIVE_INFINITY; }
-
-    double deltaY = that.y - this.y;
-
-    double deltaX = that.x - this.x;
-    if (deltaX == 0) { return Double.POSITIVE_INFINITY; }
-    if (deltaY == 0) { return 0.0; }
-
-    return deltaY/deltaX;
-  }
-
-  public void drawTo(Point that) {
-    System.out.println("drawing");
-  }
-
-  public Comparator<Point> slopeOrder() {
-    return new PointComparator(this);
-  }
-
-  private class PointComparator implements Comparator<Point> {
-    Point invokingPoint;
-
-    PointComparator(Point invokingPoint) {
-      this.invokingPoint = invokingPoint;
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
     }
 
-    public int compare(Point one, Point two) {
-      double slope1 = this.invokingPoint.slopeTo(one);
-      double slope2 = this.invokingPoint.slopeTo(two);
-
-      if (slope1 > slope2) { return 1; }
-      else if (slope1 < slope2) { return -1; }
-      else { return 0; }
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
-  }
 
-  /* public static void testCompareTo(Point input1, Point input2, int expectedResult) {
-    if (input1.compareTo(input2) != expectedResult) {
-      System.out.println("Test failed: " + input1.toString() + ", " + input2.toString());
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if(this.compareTo(that) == 0) { return Double.NEGATIVE_INFINITY; }
+
+        double deltaY = that.y - this.y;
+
+        double deltaX = that.x - this.x;
+        if (deltaX == 0) { return Double.POSITIVE_INFINITY; }
+        if (deltaY == 0) { return 0.0; }
+
+        return deltaY/deltaX;
     }
-  }
 
-  public static void testSlopeTo(Point input1, Point input2, double expectedResult) {
-    if (input1.slopeTo(input2) != expectedResult) {
-      System.out.println("Test failed: " + input1.toString() + ", " + input2.toString());
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        if (this.y < that.y) { return -1; }
+
+        if (this.y == that.y) {
+            if (this.x == that.x) { return 0; }
+            if (this.x < that.x) { return -1; }
+        }
+        
+        return 1;
     }
-  } */
 
-  /**
-  * Draws this point to standard draw.
-  */
-  public void draw() {
-    /* DO NOT MODIFY */
-    StdDraw.point(x, y);
-  }
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        return new PointComparator(this);
+    }
 
-  /**
-  * Returns a string representation of this point.
-  * This method is provide for debugging;
-  * your program should not rely on the format of the string representation.
-  *
-  * @return a string representation of this point
-  */
-  public String toString() {
-    /* DO NOT MODIFY */
-    return "(" + x + ", " + y + ")";
-  }
+    private class PointComparator implements Comparator<Point> {
+        Point invokingPoint;
 
-  public static void main(String[] args) {
-    // // CompareTo test suite
-    // testCompareTo(new Point(3, 2), new Point(2, 1), 1);
-    // testCompareTo(new Point(3, 2), new Point(2, 2), 1);
-    // testCompareTo(new Point(3, 2), new Point(3, 2), 0);
-    // testCompareTo(new Point(3, 1), new Point(2, 2), -1);
+        PointComparator(Point invokingPoint) {
+            this.invokingPoint = invokingPoint;
+        }
 
-    // // slopeTo test suite
-    // testSlopeTo(new Point(3, 3), new Point(2, 2), 1.0);
-    // testSlopeTo(new Point(3, 3), new Point(3, 3), Double.NEGATIVE_INFINITY);
-    // testSlopeTo(new Point(3, 3), new Point(6, 3), 0.0);
-    // testSlopeTo(new Point(3, 3), new Point(3, 6), Double.POSITIVE_INFINITY);
-  }
+        public int compare(Point one, Point two) {
+            double slope1 = this.invokingPoint.slopeTo(one);
+            double slope2 = this.invokingPoint.slopeTo(two);
+
+            if ((slope1 - slope2) > epsilon ) { return 1; }
+            else if ((slope1 - slope2) < -1 * epsilon) { return -1; }
+            else { return 0; }
+        }
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
 }
-
